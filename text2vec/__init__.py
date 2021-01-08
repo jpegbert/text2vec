d1f99c83160d67a0eca93e28a6ee9b7f1ca3481e
jiangpeng.jiang <jiangpeng.jiang@zhaopin.com.cn>
2021-01-08 17:19:54 +0800

    upload

---
+++ text2vec/__init__.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+@author:XuMing<xuming624@qq.com>
+@description: 
+"""
+
+from pathlib import Path
+
+from text2vec.similarity import Similarity, SearchSimilarity, SimType
+from text2vec.utils.logger import set_log_level
+from text2vec.vector import EmbType, Vector
+
+USER_DIR = Path.expanduser(Path('~')).joinpath('.text2vec')
+if not USER_DIR.exists():
+    USER_DIR.mkdir()
+USER_DATA_DIR = USER_DIR.joinpath('datasets')
+if not USER_DATA_DIR.exists():
+    USER_DATA_DIR.mkdir()
+
+VEC = Vector()
+encode = VEC.encode
+set_stopwords_file = VEC.set_stopwords_file
